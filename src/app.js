0e202313ccca72626d957cc7ff986c2b19117584
Juliansv <juliansv22@gmail.com>
2021-08-27 20:12:04 -0300

    Termina con la clase

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 
+
 const path = require('path');
 
 app.listen('3000', () => console.log('Todo bien por aca'));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
-app.get('/', )
-
-app.get('/about', )
+const mainRouter = require('./routes/main-routes')
+app.use(mainRouter);
--- src/controllers/main-controller.js
+++ src/controllers/main-controller.js
@@ -1,9 +1,11 @@
+const path = require('path');
+
 const controller = {
     home: (req, res) => {
-        res.sendFile(path.join(__dirname, 'views/home.html'));
+        res.sendFile(path.join(__dirname, '../views/home.html'));
     },
     about: (req, res) => {
-        res.sendFile(path.join(__dirname, 'views/about.html'));
+        res.sendFile(path.join(__dirname, '../views/about.html'));
     },
 };
 
