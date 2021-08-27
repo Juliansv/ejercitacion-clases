bc826d58bb28247e0b2184d7cff0af5e29ad430c
Julian pc-pagano <gregules@pagano-sa.com.ar>
2021-08-27 17:43:59 -0300

    wip

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const path = require('path');
+
+app.listen('3000', () => console.log('Todo bien por aca'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/home.html'));
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/about.html'));
+})
