82466a40c1a517790566a10f0cd7f517c3223b1a
Julian pc-pagano <gregules@pagano-sa.com.ar>
2021-08-31 15:08:04 -0300

    Agrega ejs al proyecto

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ const app = express();
 
 const path = require('path');
 
+app.set('view engine', 'ejs');
+app.set('views', path.resolve(__dirname, 'views'));
+
 app.listen('3000', () => console.log('Todo bien por aca'));
 
 app.use(express.static(path.join(__dirname, '../public')));
